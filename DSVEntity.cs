4ae38b456cedae32586f57f9867bbe46d66e27df
TaikiTkayanagi <73454596+TaikiTkayanagi@users.noreply.github.com>
2022-05-31 23:40:57 +0900

    Add files via upload

---
+++ DSVEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BinaryRead
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    class DSVEntity
+    {
+        public byte A;
+        public byte B;
+        public byte C;
+        /// <summary>
+        /// リトルエンディアンになって、値が入れられる
+        /// </summary>
+        public int D;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace BinaryRead
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string dsvPath = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\binary.dsv";
+            string dsvPath2 = @"C:\Users\81803\Desktop\binary.dsv";
+
+            var dsvProvider = new DSVProvider(dsvPath2);
+
+            var hoge = dsvProvider.DsvBytes;
+            GCHandle gch = GCHandle.Alloc(hoge, GCHandleType.Pinned);
+            DSVEntity result = (DSVEntity)Marshal.PtrToStructure(gch.AddrOfPinnedObject(), typeof(DSVEntity));
+            Console.WriteLine($"{result.D}");
+            Console.WriteLine(0x01000000);
+            gch.Free();
+        }
+    }
+}
